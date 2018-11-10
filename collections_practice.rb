988632891b15a1e09288263ea9e822af8d39b3ba
Molly McCarron <mccarronmollye@gmail.com>
2018-11-10 19:28:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,6 +14,6 @@ def remove_non_strings(array)
   array.delete_if {|word| !(word.is_a? String)}
 end
 
-def count_elements(array)
-  array {|word| word.count}
+def count_elements(array, word)
+  array.count(word)
 end

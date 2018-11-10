217b0d81923db7ae3580c754ed5eaa51d317fc50
Molly McCarron <mccarronmollye@gmail.com>
2018-11-10 19:20:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|word| word.start_with?('r')}
+end
+
+def contain_a(array)
+  array.select {|word| word.include?('a')}
+end
+
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
+end

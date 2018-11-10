0bd9e33eff4299cdcf019e4cabd8b95cb9247e54
Molly McCarron <mccarronmollye@gmail.com>
2018-11-10 19:54:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,3 +25,18 @@ def count_elements(array)
     end
   end.uniq
 end
+
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  container
+end
